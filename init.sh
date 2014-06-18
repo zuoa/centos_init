f3069441b332b7dabf34c74c81db6107d942cffd
root <root@ip-172-31-4-32.us-west-2.compute.internal>
2014-06-18 00:15:38 -0400

    first init

---
+++ init.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+############################
+# author:jedyu
+# website:http://xlolo.com
+# version:0.1
+############################
+
+init_dir=/opt/init_install/
+if [ ! -d $init_dir] 
+then
+    mkdir -p $init_dir
+fi
+
+yum update
+yum install gcc openssl openssl-devel zlib-devel git -y
+
+#update python
+cd $init_dir
+wget https://www.python.org/ftp/python/2.7.7/Python-2.7.7.tgz
+tar zvxf Python-2.7.7.tgz
+cd Python-2.7.7
+sed -i 's/#zlib/zlib/'  Modules/Setup.dist
+./configure  && make && make install
+mv /usr/bin/python /usr/bin/python_old
+ln -s /usr/local/bin/python2.7 /usr/bin/python
+sed -i 's/python/python_old/'  /usr/bin/yum
+
+#install easy_install
+cd $init_dir
+wget https://bootstrap.pypa.io/ez_setup.py
+python ez_setup.py
+
+#install pip
+easy_install pip
