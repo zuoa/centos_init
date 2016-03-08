d071473e33e1067aa9ae5965dd4da433eba48c54
zuoa <ijedyu@gmail.com>
2016-03-08 09:55:48 +0800

    update getpip

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 ############################
-# author:jedyu
-# website:http://xlolo.com
+# author:zuoa
+# website:http://yuy.im
 # version:0.1
 ############################
 
 init_dir=/opt/init_install/
-if [ ! -d $init_dir] 
+if [ ! -d $init_dir];
 then
     mkdir -p $init_dir
 fi
@@ -25,10 +25,7 @@ mv /usr/bin/python /usr/bin/python_old
 ln -s /usr/local/bin/python2.7 /usr/bin/python
 sed -i 's/python/python_old/'  /usr/bin/yum
 
-#install easy_install
-cd $init_dir
-wget https://bootstrap.pypa.io/ez_setup.py
-python ez_setup.py
-
 #install pip
-easy_install pip
+cd $init_dir
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
